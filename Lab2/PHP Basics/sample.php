fdc94fac710efec5427b49c269b9d83b490b9e08
Muhammad Fatah <ab.fatahmn@gmail.com>
2020-10-18 15:57:02 +0600

    PHP Basics

---
+++ Lab2/PHP Basics/sample.php
@@ -0,0 +1,12 @@
+<h1>PHP BASICS<h1>
+<?php
+    echo "<b>Hello World!</b>";
+    print "Hello World!";
+    echo htmlspecialchars("<b>Hello World!</b>");
+    $txt="ASP Dot Net Core";
+    $scndTxt="Is Better!";
+    $x=10;
+    $y=10.6
+    echo $txt." ".$scndTxt;
+    echo $x + $y;
+?>
